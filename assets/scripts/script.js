b4c9c52ca797236e1710e9261d3c8ee1188384b7
Kiven Alaric <elmozaro3@gmail.com>
2023-06-22 00:12:16 -0500

    formating of code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,117 +1,117 @@
-const purchase = document.getElementById("bill");
-const payment = document.getElementById("cash");
-const submitBtn = document.getElementById("submit-btn");
-const displayResult = document.getElementById("results");
-const form = document.getElementById("submission-form");
+const purchase = document.getElementById('bill')
+const payment = document.getElementById('cash')
+const submitBtn = document.getElementById('submit-btn')
+const displayResult = document.getElementById('results')
+const form = document.getElementById('submission-form')
 
 const denominations = [
-  ["PENNY", 1],
+  ['PENNY', 1],
 
-  ["NICKEL", 5],
+  ['NICKEL', 5],
 
-  ["DIME", 10],
+  ['DIME', 10],
 
-  ["QUARTER", 25],
+  ['QUARTER', 25],
 
-  ["ONE", 100],
+  ['ONE', 100],
 
-  ["FIVE", 500],
+  ['FIVE', 500],
 
-  ["TEN", 1000],
+  ['TEN', 1000],
 
-  ["TWENTY", 2000],
+  ['TWENTY', 2000],
 
-  ["ONE HUNDRED", 10000],
-];
+  ['ONE HUNDRED', 10000]
+]
 
-const penny = document.getElementById("penny");
-const nickel = document.getElementById("nickel");
-const dime = document.getElementById("dime");
-const quarter = document.getElementById("quarter");
-const one = document.getElementById("one");
-const five = document.getElementById("five");
-const ten = document.getElementById("ten");
-const twenty = document.getElementById("twenty");
-const onehundred = document.getElementById("one-hundred");
+const penny = document.getElementById('penny')
+const nickel = document.getElementById('nickel')
+const dime = document.getElementById('dime')
+const quarter = document.getElementById('quarter')
+const one = document.getElementById('one')
+const five = document.getElementById('five')
+const ten = document.getElementById('ten')
+const twenty = document.getElementById('twenty')
+const onehundred = document.getElementById('one-hundred')
 
-function checkCashRegister() {
-  let penny1 = penny.value;
-  let nickel1 = nickel.value;
-  let dime1 = dime.value;
-  let quarter1 = quarter.value;
-  let one1 = one.value;
-  let five1 = five.value;
-  let ten1 = ten.value;
-  let twenty1 = twenty.value;
-  let onehundred1 = onehundred.value;
-  let purchase1 = purchase.value;
-  let payment1 = payment.value;
+function checkCashRegister () {
+  const penny1 = penny.value
+  const nickel1 = nickel.value
+  const dime1 = dime.value
+  const quarter1 = quarter.value
+  const one1 = one.value
+  const five1 = five.value
+  const ten1 = ten.value
+  const twenty1 = twenty.value
+  const onehundred1 = onehundred.value
+  const purchase1 = purchase.value
+  const payment1 = payment.value
   const cid = [
-    ["PENNY", penny1],
-    ["NICKEL", nickel1],
-    ["DIME", dime1],
-    ["QUARTER", quarter1],
-    ["ONE", one1],
-    ["FIVE", five1],
-    ["TEN", ten1],
-    ["TWENTY", twenty1],
-    ["ONE HUNDRED", onehundred1],
-  ];
-  console.log(purchase1, payment1);
+    ['PENNY', penny1],
+    ['NICKEL', nickel1],
+    ['DIME', dime1],
+    ['QUARTER', quarter1],
+    ['ONE', one1],
+    ['FIVE', five1],
+    ['TEN', ten1],
+    ['TWENTY', twenty1],
+    ['ONE HUNDRED', onehundred1]
+  ]
+  console.log(purchase1, payment1)
 
-  let change = Math.round(payment1 * 100 - purchase1 * 100);
-  let cashInHand = {};
-  let cashToGive = {};
+  let change = Math.round(payment1 * 100 - purchase1 * 100)
+  const cashInHand = {}
+  const cashToGive = {}
 
   cid.forEach((denominations) => {
-    cashInHand[denominations[0]] = Math.round(denominations[1] * 100);
-  });
+    cashInHand[denominations[0]] = Math.round(denominations[1] * 100)
+  })
 
-  let index = denominations.length - 1;
+  let index = denominations.length - 1
 
   while (index >= 0 && change > 0) {
-    let moneyName = denominations[index][0];
-    let moneyValue = denominations[index][1];
+    const moneyName = denominations[index][0]
+    const moneyValue = denominations[index][1]
     if ((change - moneyValue > 0 && cashInHand[moneyName], change)) {
-      cashToGive[moneyName] = 0;
+      cashToGive[moneyName] = 0
       while (cashInHand[moneyName] > 0 && change - moneyValue >= 0) {
-        cashInHand[moneyName] -= moneyValue;
-        cashToGive[moneyName] += moneyValue;
-        change -= moneyValue;
+        cashInHand[moneyName] -= moneyValue
+        cashToGive[moneyName] += moneyValue
+        change -= moneyValue
       }
     }
-    index -= 1;
+    index -= 1
   }
 
   if (change === 0) {
-    let emptyRegister = true;
+    let emptyRegister = true
 
     Object.keys(cashInHand).forEach((moneyType) => {
       if (cashInHand[moneyType] > 0) {
-        emptyRegister = false;
+        emptyRegister = false
       }
-    });
+    })
 
     if (emptyRegister) {
       return {
-        status: "CLOSED",
-        change: cid,
-      };
+        status: 'CLOSED',
+        change: cid
+      }
     } else {
-      let changeArray = [];
+      const changeArray = []
       Object.keys(cashToGive).map((moneyType) => {
         if (cashToGive[moneyType] > 0) {
-          changeArray.push([moneyType, cashToGive[moneyType] / 100]);
+          changeArray.push([moneyType, cashToGive[moneyType] / 100])
         }
-      });
-      return { status: "OPEN", change: changeArray };
+      })
+      return { status: 'OPEN', change: changeArray }
     }
   }
-  return { status: "INSUFFICIENT_FUNDS", change: [] };
+  return { status: 'INSUFFICIENT_FUNDS', change: [] }
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let res = checkCashRegister();
-  displayResult.innerHTML = `{ staus: ${res.status}, change: ${res.change}}`;
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const res = checkCashRegister()
+  displayResult.innerHTML = `{ staus: ${res.status}, change: ${res.change}}`
+})
